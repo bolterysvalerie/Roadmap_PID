2ee8294be92f5dc31e49d694ab58541b08a78289
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2024-11-06 18:26:59 +0100

    Form create Artist

--- src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import be.iccbxl.pid.reservationsspringboot.model.Artist;
 import be.iccbxl.pid.reservationsspringboot.service.ArtistService;
@@ -76,6 +73,27 @@ public class ArtistController {
         }
 
         service.updateArtist(id, artist);
+
+        return "redirect:/artists/"+artist.getId();
+    }
+
+    @GetMapping("/artists/create")
+    public String create(Model model) {
+        Artist artist = new Artist(null,null);
+
+        model.addAttribute("artist", artist);
+
+        return "artist/create";
+    }
+
+    @PostMapping("/artists/create")
+    public String store(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "artist/create";
+        }
+
+        service.addArtist(artist);
 
         return "redirect:/artists/"+artist.getId();
     }
