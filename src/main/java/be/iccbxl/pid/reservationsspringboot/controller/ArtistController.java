04d520cc2e1f3ea6a67b2d165f09a30b2c2a4fed
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2024-11-06 18:53:28 +0100

    Form Delete Artist

--- src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
@@ -98,6 +98,17 @@ public class ArtistController {
         return "redirect:/artists/"+artist.getId();
     }
 
+    @DeleteMapping("/artists/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
+        Artist existing = service.getArtist(id);
+
+        if(existing!=null) {
+            service.deleteArtist(id);
+        }
+
+        return "redirect:/artists";
+    }
+
 
 }
 
