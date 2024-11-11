3cd832215c295ac449d20c67b478097376a7970a
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2024-11-11 11:43:16 +0100

    Models, controllers, templates and routes for Type and Locality

--- src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
@@ -2,7 +2,6 @@ package be.iccbxl.pid.reservationsspringboot.controller;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
