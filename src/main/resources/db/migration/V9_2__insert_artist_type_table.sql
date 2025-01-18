0da871c05e2cee7c0a54cdee9a5bc584f34dc33c
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-01-18 15:41:08 +0100

    Insert datas in table artist_type

---
+++ src/main/resources/db/migration/V9_2__insert_artist_type_table.sql
@@ -0,0 +1,20 @@
+INSERT INTO `artist_type` (`id`, `artist_id`, `type_id`) VALUES
+                                                             (1, 2, 1),  -- Philippe Laurent
+                                                             (2, 3, 1),  -- Marius Von Mayenburg
+                                                             (3, 2, 2),  -- Philippe Laurent
+                                                             (4, 3, 2),  -- Marius Von Mayenburg
+                                                             (5, 14, 3), -- Daniel Marcelin
+                                                             (6, 6, 1),  -- Pietro Varasso
+                                                             (7, 7, 2),  -- Laurent Caron
+                                                             (8, 8, 3),  -- Élena Perez
+                                                             (9, 9, 3),  -- Guillaume Alexandre
+                                                             (10, 10, 3), -- Claude Semal
+                                                             (11, 11, 3), -- Laurence Warin
+                                                             (12, 12, 3), -- Pierre Wayburn
+                                                             (13, 13, 1), -- Gwendoline Gauthier
+                                                             (14, 14, 2), -- Daniel Marcelin
+                                                             (15, 15, 3), -- Bob Marley
+                                                             (16, 16, 1), -- Maria Carey
+                                                             (17, 12, 3), -- Pierre Wayburn
+                                                             (18, 13, 3), -- Gwendoline Gauthier
+                                                             (19, 14, 3); -- Daniel Marcelin
