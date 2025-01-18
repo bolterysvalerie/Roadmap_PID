97a515b8425c6837bbe4a164c5d2cc38ee402a49
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-01-18 17:14:06 +0100

    Update ShowController avec méthode pour R©cupérer les artistes et les grouper par type

--- src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ShowController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ShowController.java
@@ -1,7 +1,12 @@
 package be.iccbxl.pid.reservationsspringboot.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import be.iccbxl.pid.reservationsspringboot.model.Artist;
+import be.iccbxl.pid.reservationsspringboot.model.ArtistType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,6 +34,21 @@ public class ShowController {
     @GetMapping("/shows/{id}")
     public String show(Model model, @PathVariable("id") String id) {
         Show show = service.get(id);
+
+        //Récupérer les artistes du spectacle et les grouper par type
+        Map<String, ArrayList<Artist>> collaborateurs = new TreeMap<>();
+
+        for(ArtistType at : show.getArtistTypes()) {
+            String type = at.getType().getType();
+
+            if(collaborateurs.get(type) == null) {
+                collaborateurs.put(type, new ArrayList<>());
+            }
+
+            collaborateurs.get(type).add(at.getArtist());
+        }
+
+        model.addAttribute("collaborateurs", collaborateurs);
 
         model.addAttribute("show", show);
         model.addAttribute("title", "Fiche d'un spectacle");
