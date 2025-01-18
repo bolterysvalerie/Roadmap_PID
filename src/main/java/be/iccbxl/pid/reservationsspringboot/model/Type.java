59e236c40baef73b4e8834aa1686b3595a8f8d1d
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-01-18 15:38:45 +0100

    Update Model Type + ManyToMany Relation with artist_type and add methods

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/Type.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Type.java
@@ -1,10 +1,9 @@
 package be.iccbxl.pid.reservationsspringboot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="types")
@@ -13,6 +12,14 @@ public class Type {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String type;
+
+    @ManyToMany
+    @JoinTable(
+            name = "artist_type",
+            joinColumns = @JoinColumn(name = "type_id"),
+            inverseJoinColumns = @JoinColumn(name = "artist_id"))
+    private List<Artist> artists = new ArrayList<>();
+
 
     protected Type() { }
 
@@ -31,6 +38,29 @@ public class Type {
     public void setType(String type) {
         this.type = type;
     }
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public Type addArtist(Artist artist) {
+        if(!this.artists.contains(artist)) {
+            this.artists.add(artist);
+            artist.addType(this);
+        }
+
+        return this;
+    }
+
+    public Type removeType(Artist artist) {
+        if(this.artists.contains(artist)) {
+            this.artists.remove(artist);
+            artist.getTypes().remove(this);
+        }
+
+        return this;
+    }
+
 
     @Override
     public String toString() {
