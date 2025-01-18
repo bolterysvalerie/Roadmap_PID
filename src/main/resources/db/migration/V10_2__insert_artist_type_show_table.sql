db4e25f0e59c27c668315eda4f7621949658ff19
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-01-18 17:03:05 +0100

    insertion des données dans la table artist_type_show

---
+++ src/main/resources/db/migration/V10_2__insert_artist_type_show_table.sql
@@ -0,0 +1,20 @@
+INSERT INTO `artist_type_show` (`id`, `show_id`, `artist_type_id`) VALUES
+                                                                       (1, 1, 1),  -- Philippe Laurent, type 1
+                                                                       (2, 1, 2),  -- Marius Von Mayenburg, type 1
+                                                                       (3, 1, 3),  -- Philippe Laurent, type 2
+                                                                       (4, 1, 4),  -- Marius Von Mayenburg, type 2
+                                                                       (5, 1, 5),  -- Daniel Marcelin, type 3
+                                                                       (6, 2, 6),  -- Pietro Varasso, type 1
+                                                                       (7, 2, 7),  -- Laurent Caron, type 2
+                                                                       (8, 2, 8),  -- Élena Perez, type 3
+                                                                       (9, 2, 9),  -- Guillaume Alexandre, type 3
+                                                                       (10, 2, 10), -- Claude Semal, type 3
+                                                                       (11, 2, 11), -- Laurence Warin, type 3
+                                                                       (12, 2, 12), -- Pierre Wayburn, type 3
+                                                                       (13, 3, 13), -- Gwendoline Gauthier, type 1
+                                                                       (14, 3, 14), -- Daniel Marcelin, type 2
+                                                                       (15, 3, 15), -- Bob Marley, type 3
+                                                                       (16, 4, 16), -- Maria Carey, type 1
+                                                                       (17, 4, 17), -- Pierre Wayburn, type 3
+                                                                       (18, 4, 18), -- Gwendoline Gauthier, type 3
+                                                                       (19, 4, 19); -- Daniel Marcelin, type 3
