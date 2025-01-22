99fd763668436f8f27c02e7548ee2148d5d054d7
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-01-22 19:59:38 +0100

    Update des données de la table reservations

--- src/main/resources/db/migration/V13_2__insert_reservations_table.sql
+++ src/main/resources/db/migration/V13_2__insert_reservations_table.sql
@@ -1,3 +1,12 @@
-INSERT INTO `reservations` (`id`, `user_id`, `representation_id`, `places`) VALUES
-                                                                                (NULL, 2, 1, 3),
-                                                                                (NULL, 2, 2, 5);
+INSERT INTO `reservations` (`id`, `booking_date`, `user_id`, `status`)
+VALUES
+    (1, '2025-01-06 10:00:00', 1, 'Confirmed'),
+    (2, '2025-01-06 11:00:00', 2, 'Pending'),
+    (3, '2025-01-06 12:00:00', 3, 'Cancelled'),
+    (4, '2025-01-06 13:00:00', 4, 'Confirmed'),
+    (5, '2025-01-06 14:00:00', 5, 'Pending'),
+    (6, '2025-01-06 15:00:00', 6, 'Confirmed'),
+    (7, '2025-01-06 16:00:00', 7, 'Cancelled'),
+    (8, '2025-01-06 17:00:00', 8, 'Pending'),
+    (9, '2025-01-06 18:00:00', 9, 'Confirmed'),
+    (10, '2025-01-06 19:00:00', 10, 'Pending');
