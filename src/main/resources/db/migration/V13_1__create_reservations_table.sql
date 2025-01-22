9562c7b999772095e4cbcefb48536d5bfbda4097
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-01-22 19:58:57 +0100

    Update pour la création de la table reservations

--- src/main/resources/db/migration/V13_1__create_reservations_table.sql
+++ src/main/resources/db/migration/V13_1__create_reservations_table.sql
@@ -1,25 +1,13 @@
-CREATE TABLE `reservations` (
-                                `id` int(11) NOT NULL AUTO_INCREMENT,
-                                `user_id` int(11) NOT NULL,
-                                `representation_id` int(11) NOT NULL,
-                                `places` int(11) NOT NULL,
-                                PRIMARY KEY(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS reservations (
+                                            id BIGINT NOT NULL AUTO_INCREMENT,
+                                            booking_date TIMESTAMP NULL DEFAULT NULL,
+                                            status VARCHAR(60),
+                                            PRIMARY KEY (id)
+);
 
---
--- Index pour la table `reservations`
---
-ALTER TABLE `reservations`
-    ADD KEY `reservations_user_id` (`user_id`);
+ALTER TABLE reservations
+    ADD COLUMN user_id BIGINT NOT NULL AFTER id;
 
-ALTER TABLE `reservations`
-    ADD KEY `reservations_representation_id` (`representation_id`);
-
---
--- Contraintes pour la table `reservations`
---
-ALTER TABLE `reservations`
-    ADD CONSTRAINT `reservations_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT;
-
-ALTER TABLE `reservations`
-    ADD CONSTRAINT `reservations_representation_id` FOREIGN KEY (`representation_id`) REFERENCES `representations` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT;
+ALTER TABLE reservations
+    ADD CONSTRAINT fk_reservations_user FOREIGN KEY (user_id)
+        REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE;
