51524c2214a02be9c568773f0ee4f646ae045d40
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-01-22 20:10:22 +0100

    Insertion des données dans la table representation_reservation

---
+++ src/main/resources/db/migration/V14_2__insert_representation_reservation_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO `representation_reservations` (`id`, `quantity`, `price_id`, `representation_id`, `reservation_id`)
+VALUES
+    (1, 2, 1, 1, 1),
+    (2, 3, 2, 2, 2),
+    (3, 1, 3, 3, 3),
+    (4, 4, 4, 4, 4),
+    (5, 2, 5, 5, 5),
+    (6, 3, 6, 6, 6),
+    (7, 1, 7, 7, 7),
+    (8, 5, 8, 8, 8),
+    (9, 2, 9, 9, 9),
+    (10, 4, 10, 10, 10);
