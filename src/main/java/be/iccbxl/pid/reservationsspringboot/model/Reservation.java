e87dff659350cd408610de505963f49f00774ad2
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-01-22 20:01:53 +0100

    Création du Model Reservation avec Mapping OneToMany avec RepresentationReservation

---
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Reservation.java
@@ -0,0 +1,35 @@
+package be.iccbxl.pid.reservationsspringboot.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data @NoArgsConstructor
+@Entity
+@Getter @Setter
+@Table(name = "reservations")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "booking_date")
+    private LocalDateTime bookingDate;
+    @Column(name = "status", length = 60)
+    private String status;
+
+    // Relation One To Many
+    @OneToMany(mappedBy = "reservation")
+    private Set<RepresentationReservation> representation_reservation;
+
+    // Relation Many To One
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+}
