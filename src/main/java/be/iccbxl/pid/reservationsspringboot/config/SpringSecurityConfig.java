4020297200d64ede6398543f6f9738a5d10dfc82
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-02-26 12:45:02 +0100

    Mise à jour avant API

--- src/main/java/be/iccbxl/pid/reservationsspringboot/config/SpringSecurityConfig.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/config/SpringSecurityConfig.java
@@ -10,6 +10,59 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+/*@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+   //public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+        return http.cors(Customizer.withDefaults())
+                // Désactiver la protection des form
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    //auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers("/admin").hasRole("ADMIN");
+                    auth.requestMatchers("/user").hasRole("MEMBER");
+                    //API
+                    auth.requestMatchers("/api/public/**").permitAll(); //Endpoints publics
+                    auth.requestMatchers("/api/admin/**").hasRole("ADMIN"); //Endpoints admin
+                    auth.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults()) // Authentification de base (utile pour les tests)
+                //.formLogin(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .usernameParameter("login")
+                        .failureUrl("/login?loginError=true"))
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logoutSuccess=true")
+                        .deleteCookies("JSESSIONID"))
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login?loginRequired=true")))
+               // .rememberMe(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+        AuthenticationManagerBuilder authMngrBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authMngrBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
+        return authMngrBuilder.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+ */
 
 @Configuration
 @EnableWebSecurity
